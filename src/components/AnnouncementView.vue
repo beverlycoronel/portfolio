f10a5b76ecb8090c9b9604e156e3631d2cab6499
Beverly C <bcoronel96@gmail.com>
2023-10-12 02:37:37 -0700

    temp banner

--- src/components/AnnouncementView.vue
+++ src/components/AnnouncementView.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p class="text-sm leading-6 text-gray-900">
-          <strong class="font-semibold">I'm attending GHC '23!</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Please be patient as I'm constantly making changes and updating this page in preparation for the conference. 
+          <strong class="font-semibold">I'm attending WE '23!</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Please be patient as I'm constantly making changes and updating this page in preparation for the conference. 
         </p>
         <!-- <a href="https://www.linkedin.com/in/beverlycoronel" class="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">Connect with me <span aria-hidden="true">&rarr;</span></a> -->
       </div>
